1eda887c129198f756c4ea87e700cd43d5fd9c36
kappanneo <pannacciclaudio@gmail.com>
2021-12-07 19:24:33 +0100

    executable template

---
+++ execs/template.hs
@@ -0,0 +1,11 @@
+import System.Environment
+
+import qualified Klaus as Santa
+
+main :: IO ()
+main = do -- IO
+   args <- getArgs
+   printout <- readFile $ if null args then "inputs/.txt" else args!!0
+   let p1 = 0 :: Result
+   let p2 = 0 :: Result
+   Santa.takeCreditForElvesUnderpaidWork p1 p2
