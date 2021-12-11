e26922b63e9affa66ca50908f05fec2cf25a6697
kappanneo <pannacciclaudio@gmail.com>
2021-12-11 10:42:27 +0100

    the missing piece

---
+++ doctests.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE BlockArguments #-}
+
+import Data.Foldable ( for_ )
+import Data.List ( sort, isPrefixOf )
+import System.Directory ( listDirectory, Permissions (executable) )
+import System.Directory.Extra ( listDirectory, listFilesRecursive )
+import System.FilePath ( (</>), takeBaseName, takeExtension )
+import System.IO ( stderr, hPutStrLn )
+import Test.DocTest ( doctest )
+
+main :: IO ()
+main = do -- IO
+   files <- onlyExecutables
+   for_ files \file -> do -- IO
+      hPutStrLn stderr ("\nTesting " ++ file)
+      doctest [file]
+
+allSourceFiles :: IO [FilePath]
+allSourceFiles = filter (\x -> takeExtension x == ".hs") . sort 
+   <$> listFilesRecursive "src"
+
+onlyExecutables :: IO [FilePath]
+onlyExecutables = filter (isPrefixOf "src/y") <$> allSourceFiles
