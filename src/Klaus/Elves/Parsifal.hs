ac5615392dae02fbc7aeeb731b0b3d7806ee5216
kappanneo <pannacciclaudio@gmail.com>
2021-12-11 21:49:58 +0100

    reanamed parse to lineParse

--- src/Klaus/Elves/Parsifal.hs
+++ src/Klaus/Elves/Parsifal.hs
@@ -30,11 +30,11 @@ uppercase (c:cs) = toUpper c : cs
 
 class Read a => Parsiable a where
 
-   parse :: String -> a
-   parse = read
+   lineParse :: String -> a
+   lineParse = read
 
    fileParse :: String -> a
-   fileParse = parse
+   fileParse = lineParse
 
    parseFile :: FilePath -> IO a
    parseFile file = do -- IO
@@ -44,28 +44,28 @@ class Read a => Parsiable a where
 instance Parsiable Number
 
 instance Parsiable [Number] where
-   parse s = read ("[" ++ s ++ "]")
-   fileParse = map parse . lines
+   lineParse s = read ("[" ++ s ++ "]")
+   fileParse = map read . lines
 
 instance Parsiable Submarine.Command where
-   parse = read . uppercase
+   lineParse = read . uppercase
 
 instance Parsiable Submarine.Program  where
-   fileParse = map parse . lines
+   fileParse = map lineParse . lines
 
 instance Parsiable Data.Bin
 
 instance Parsiable Data.Diagnostics where
-   fileParse = map parse . lines
+   fileParse = map lineParse . lines
 
 instance Parsiable Bingo.Slot where
-   parse = fromNumber . read
+   lineParse = fromNumber . read
 
 instance Parsiable Bingo.Board where
-   parse = chunksOf 5 . map parse . take 25 . words
+   fileParse = chunksOf 5 . map fileParse . take 25 . words
 
 instance Parsiable Bingo.Round where
    parseFile file = do -- IO
       s <- readFile file
       let (ns:bs) = splitOn "\n\n" s
-      return (parse ns, map fileParse bs)
+      return (lineParse ns, map fileParse bs)
